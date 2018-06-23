a768d1d0cd41ea03852911d2c355b0582cfb59be
xzgz <heyanguang@yahoo.com>
2018-06-23 16:07:41 +0800

    1

--- siamese_tf_mnist/siamese_resnet_train.py
+++ siamese_tf_mnist/siamese_resnet_train.py
@@ -87,7 +87,6 @@ def train_siamese():
         batch_x1, batch_y1 = mnist.train.next_batch(128)
         batch_x2, batch_y2 = mnist.train.next_batch(128)
         batch_y = (batch_y1 == batch_y2).astype('float')
-        print('batch_y:\n', batch_y)
 
         _, loss_v, gs_v, lr_v = sess.run([train_step, siamese.loss, global_step, lr], feed_dict={
             siamese.x1: batch_x1,
@@ -98,6 +97,7 @@ def train_siamese():
             print('Model diverged with loss = NaN')
             quit()
         if iterations % 500 == 0:
+            print('batch_y:\n', batch_y)
             # print('step %d: loss %.3f' % (iterations, loss_v))
             print('Global step: {:d}, iterations: {:d}, learning rate: {:.5f}, loss: {:.4f}'.format(
                 gs_v, iterations, lr_v, loss_v))
