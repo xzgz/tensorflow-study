0408e39bf15ee2d4497cca1938e33a68e565a447
xzgz <heyanguang@yahoo.com>
2018-06-24 18:16:54 +0800

    1

--- siamese_tf_mnist/siamese_resnet_model.py
+++ siamese_tf_mnist/siamese_resnet_model.py
@@ -31,7 +31,8 @@ class Siamese:
         # self.loss = self.loss_cross_entropy()
         self.distance = self.pair_distance()
         self.single_sample_identity = tf.argmax(-self.distance, 0)
-        self.classify_features = self.cnn_classify_model(self.classify_images, self.is_training, scope_reuse=False)
+        # self.classify_features = self.cnn_classify_model(self.classify_images, self.is_training, scope_reuse=False)
+        self.classify_features = None
         self.classify_loss = self.loss_classify(self.classify_features, self.classify_labels)
         self.predicted_labels = self.classify_predict(self.classify_features)
 
