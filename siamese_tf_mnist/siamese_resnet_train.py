478fedf2e0b977952efbc903f820a2ccf99a1284
xzgz <heyanguang@yahoo.com>
2018-06-24 12:16:21 +0800

    1

--- siamese_tf_mnist/siamese_resnet_train.py
+++ siamese_tf_mnist/siamese_resnet_train.py
@@ -142,7 +142,7 @@ def train_siamese_resnet50():
     global_step = tf.Variable(0, name='global_step_m', trainable=False)
     lr = tf.train.piecewise_constant(global_step, boundaries, learning_rates)
     siamese = siamese_resnet_model_50.Siamese(is_training=True)
-    train_step = tf.train.GradientDescentOptimizer(0.1).minimize(siamese.loss, global_step=global_step)
+    train_step = tf.train.GradientDescentOptimizer(lr).minimize(siamese.loss, global_step=global_step)
     saver = tf.train.Saver()
 
     mnist = input_data.read_data_sets('data/mnist-data', one_hot=False)
@@ -161,6 +161,7 @@ def train_siamese_resnet50():
             break
 
     sess = tf.InteractiveSession()
+    tf.global_variables_initializer().run()
     if model_snapshot_path is None:
         tf.global_variables_initializer().run()
     else:
@@ -188,7 +189,7 @@ def train_siamese_resnet50():
     accuracy = correct_count / (test_images_num-100)
 
     print('The initial loss:', initial_loss)
-    print('Global step:', sess.run(global_step))
+    # print('Global step:', sess.run(global_step))
     print('Initial learning rate:', sess.run(lr))
     print('Initial accuracy: {:.4f}'.format(accuracy))
 
