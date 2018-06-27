9fc94d9653d8a7cd6fb949573efe2bdc419163e7
xzgz <heyanguang@yahoo.com>
2018-06-27 10:40:31 +0800

    1

--- siamese_tf_mnist/siamese_resnet_train.py
+++ siamese_tf_mnist/siamese_resnet_train.py
@@ -41,12 +41,12 @@ snapshot = 'variables'
 # model_snapshot_path = os.path.join(model_save_dir, snapshot)
 model_snapshot_path = None
 
-# learning_rates = [0.01, 0.001, 0.0001]
-learning_rates = [10.0, 1.0, 0.1]
+learning_rates = [0.1, 0.01, 0.001]
+# learning_rates = [10.0, 1.0, 0.1]
 # start_iterations = 92000
 start_iterations = 0
 max_iterations = 120000
-boundaries = [40000, 80000]
+boundaries = [20000, 80000]
 
 
 def train_siamese_resnet():
